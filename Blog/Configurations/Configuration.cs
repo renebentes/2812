5693304e9ff7dbc72bf395e462bd59555d398048
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-10-30 21:42:49 -0300

    feat: Remove valores nulos

--- Blog/Configurations/Configuration.cs
+++ Blog/Configurations/Configuration.cs
@@ -2,11 +2,11 @@ namespace Blog.Configurations;
 
 public static class Configuration
 {
-    public static string ApiKey { get; set; } = null!;
+    public static string ApiKey { get; set; } = string.Empty;
 
-    public static string ApiKeyName { get; set; } = null!;
+    public static string ApiKeyName { get; set; } = string.Empty;
 
-    public static string JwtKey { get; set; } = null!;
+    public static string JwtKey { get; set; } = string.Empty;
 
-    public static SmtpConfiguration SmtpConfiguration { get; set; } = null!;
+    public static SmtpConfiguration SmtpConfiguration { get; set; } = new();
 }
--- Blog/Extensions/ConfigurationManagerExtensions.cs
+++ Blog/Extensions/ConfigurationManagerExtensions.cs
@@ -4,9 +4,9 @@ public static class ConfigurationManagerExtensions
 {
     public static void LoadConfiguration(this ConfigurationManager configuration)
     {
-        Configuration.ApiKey = configuration.GetValue<string>(nameof(Configuration.ApiKey));
-        Configuration.ApiKeyName = configuration.GetValue<string>(nameof(Configuration.ApiKeyName));
-        Configuration.JwtKey = configuration.GetValue<string>(nameof(Configuration.JwtKey));
+        Configuration.ApiKey = configuration.GetValue<string>(nameof(Configuration.ApiKey)) ?? string.Empty;
+        Configuration.ApiKeyName = configuration.GetValue<string>(nameof(Configuration.ApiKeyName)) ?? string.Empty;
+        Configuration.JwtKey = configuration.GetValue<string>(nameof(Configuration.JwtKey)) ?? string.Empty;
 
         var smtpConfiguration = new SmtpConfiguration();
         configuration.GetSection(nameof(Configuration.SmtpConfiguration)).Bind(smtpConfiguration);
